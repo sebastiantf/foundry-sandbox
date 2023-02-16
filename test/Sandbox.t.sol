ed14154d3dc9c26569cec3b28ac8a31d6f1cd5b8
Sebastian T F <sebastian.tharakan97@gmail.com>
2023-02-16 14:55:50 +0530

    feat: ✨ setup `Sandbox`

---
+++ test/Sandbox.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/Sandbox.sol";
+
+contract SandboxTest is Test {
+    Sandbox public sandbox;
+
+    function setUp() public {
+        sandbox = new Sandbox();
+    }
+}
